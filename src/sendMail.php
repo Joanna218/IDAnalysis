3d28cf5f5c7149a89a4144cd1429398b5bf70f2b
joanna <okokis101@gmail.com>
2021-06-16 23:59:25 +0800

    最後版本修正

--- src/sendMail.php
+++ src/sendMail.php
@@ -41,37 +41,6 @@
     備註說明：$note <br>
     ";
 
-    // $message = "
-    // <html>
-    //   <head>
-    //     <title>HTML email</title>
-    //   </head>
-    //   <body>
-    //     <p>iBuzz報名聯絡資料</p>
-    //     <table border='1'>
-    //     <tr>
-    //       <th>聯絡人</th>
-    //       <th>職稱</th>
-    //       <th>聯絡電話</th>
-    //       <th>公司/組織單位</th>
-    //       <th>E-mail</th>
-    //       <th>服務需求</th>
-    //       <th>備註說明</th>
-    //     </tr>
-    //     <tr>
-    //       <td style='text-align:center;'>$contactPerson</td>
-    //       <td style='text-align:center;'>$department</td>
-    //       <td style='text-align:center;'>$contactPhone</td>
-    //       <td style='text-align:center;'>$companyOrg</td>
-    //       <td style='text-align:center;'>$email</td>
-    //       <td style='text-align:center;'>$selectDemand</td>
-    //       <td style='text-align:center;'>$note</td>
-    //     </tr>
-    //     </table>
-    //   </body>
-    // </html>
-    // ";
-
     // Always set content-type when sending HTML email
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
